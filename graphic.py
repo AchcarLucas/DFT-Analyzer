16f414f189f30bf3c65a523ae493d6ee3ae85e79
Lucas Achcar <achcarlucas@gmail.com>
2020-09-12 16:48:38 -0300

    add mplstyle e license.py

---
+++ graphic.py
@@ -0,0 +1,19 @@
+'''
+    Module: Graphic
+    Author: Lucas Campos Achcar
+'''
+
+import numpy as np
+import matplotlib.pyplot as plot
+import matplotlib as mpl
+
+'''
+    Versão do Arquivo graphic.py
+    Function: _version
+    Args:
+        None
+    Return:
+        None
+'''
+def _version():
+    return '0.0.1'
